36a73a3a08b42eebf457c664d4da630b6f8d8d38
Brent Jeffson Florendo <bjflorendo@indracompany.com>
2023-07-14 10:08:01 +0800

    Add marketing deployment config

--- marketing/configs/webpack.prod.js
+++ marketing/configs/webpack.prod.js
@@ -1,12 +1,13 @@
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common')
+const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
 const prodConfig = {
   mode: 'production',
   output: {
-    filename: '[name].[contenthash].js' // [contenthash] is for caching issues
+    filename: '[name].[contenthash].js', // [contenthash] is for caching issues
+    publicPath: '/marketing/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
@@ -17,7 +18,7 @@ const prodConfig = {
       },
       shared: packageJson.dependencies,
     })
-  ]
+  ],
 }
 
 module.exports = merge(commonConfig, prodConfig);
