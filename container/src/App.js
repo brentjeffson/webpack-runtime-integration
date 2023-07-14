da9b1c36c57763defd384e49be08dc27efce25db
Brent Jeffson Florendo <bjflorendo@indracompany.com>
2023-07-14 09:58:40 +0800

    make changes

--- container/src/App.js
+++ container/src/App.js
@@ -2,5 +2,8 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-  return <MarketingApp/>
+  return <>
+    <h1>Hello, World!</h1>
+    <MarketingApp/>
+  </>
 };
