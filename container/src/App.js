d1ed1aa758d227cca0509b81ff85964bb91f847e
Brent Jeffson Florendo <bjflorendo@indracompany.com>
2023-07-14 15:40:58 +0800

    add dashboard

--- container/src/App.js
+++ container/src/App.js
@@ -1,5 +1,5 @@
-import React, { lazy, Suspense, useState } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { lazy, Suspense, useState, useEffect } from "react";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 
 import Header from "./components/Header";
 import Progress from "./components/Progress";
@@ -8,22 +8,37 @@ import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
+import { createBrowserHistory } from "history";
 
 const MarketingLazy = lazy(() => import("./components/MarketingApp"));
 const AuthLazy = lazy(() => import("./components/AuthApp"));
+const DashboardLazy = lazy(() => import("./components/DashboardApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "cn",
 });
 
+const history = createBrowserHistory();
+
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push("/dashboard");
+    }
+  }, [isSignedIn]);
+
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
         <div>
-          <Header isSignedIn={isSignedIn} onSignOut={() => { setIsSignedIn(false)}} />
+          <Header
+            isSignedIn={isSignedIn}
+            onSignOut={() => {
+              setIsSignedIn(false);
+            }}
+          />
           <Suspense fallback={<Progress />}>
             <Switch>
               <Route path="/auth">
@@ -33,11 +48,15 @@ export default () => {
                   }}
                 />
               </Route>
+              <Route path="/dashboard">
+                {!isSignedIn && <Redirect to="/" />}
+                <DashboardLazy />
+              </Route>
               <Route path="/" component={MarketingLazy} />
             </Switch>
           </Suspense>
         </div>
       </StylesProvider>
-    </BrowserRouter>
+    </Router>
   );
 };
---
+++ container/src/components/DashboardApp.js
@@ -0,0 +1,15 @@
+import { mount } from "dashboard/DashboardApp";
+import React, { useRef, useEffect } from 'react';
+
+// Used to render the marketing app
+// as directly calling the mount function in the App.js
+// is not possible.
+export default () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    mount(ref.current);
+  }, []);
+
+  return <div ref={ref}></div>;
+};
